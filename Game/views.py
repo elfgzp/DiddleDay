412c8272c4d55ffcc3ce08de60310250ce51b26e
Gzp_ <741424975@qq.com>
2016-10-10 22:07:31 +0800

    multimaster
    增加多主线游戏功能

--- Game/views.py
+++ Game/views.py
@@ -15,6 +15,8 @@ def index(request):
     start_time = "2016-10-10 00:00:00"
     start_time_s = time.mktime(time.strptime(start_time, "%Y-%m-%d %H:%M:%S"))
     try:
+        link_uuid = request.GET.get('link_uuid')
+        parameter = '?link_uuid=%s' % link_uuid if link_uuid else ''
         if start_time_s - time.time() > 0:
             return render(request, 'countdown.html', {'left_time': start_time_s - time.time()})
         return render(request, 'index.html', locals())
@@ -31,11 +33,19 @@ def descriptions(request):
 
 def step_1(request):
     try:
-        first_puzzle = Puzzle.objects.filter(previous_puzzle=None).first()
-        has_first_puzzle = False
-        if first_puzzle:
-            has_first_puzzle = True
-        return render(request, 'step1.html', locals())
+        link_uuid = request.GET.get('link_uuid')
+        if link_uuid:
+            first_puzzle = Puzzle.objects.filter(link_uuid=link_uuid).first()
+            has_first_puzzle = False
+            if first_puzzle:
+                has_first_puzzle = True
+            return render(request, 'step1.html', locals())
+        else:
+            first_puzzle = Puzzle.objects.filter(previous_puzzle=None).first()
+            has_first_puzzle = False
+            if first_puzzle:
+                has_first_puzzle = True
+            return render(request, 'step1.html', locals())
     except Exception as e:
         game_log.exception(e)
 
@@ -53,11 +63,18 @@ def step_n(request):
                 if puzzle.previous_puzzle:
                     return render(request, 'stepn.html', locals())
                 else:
-                    return redirect('index.html')
+                    return redirect('index.html?link_uuid=%s' % link_uuid)
             else:
                 return HttpResponse('404')
         else:
             return HttpResponse('404')
+    except Exception as e:
+        game_log.exception(e)
+
+
+def feed(request):
+    try:
+        return render(request, 'feed.html', locals())
     except Exception as e:
         game_log.exception(e)
 

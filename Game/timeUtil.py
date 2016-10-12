7a72fd4455680bd701de4a60d44721c812835b16
Ruter <ruter.lu@gmail.com>
2016-10-12 09:22:54 +0800

    Add time utility

---
+++ Game/timeUtil.py
@@ -0,0 +1,31 @@
+#-*- coding: utf-8 -*
+import time
+
+start_time = "2016-10-10 00:00:00"
+start_time_s = time.mktime(time.strptime(start_time, "%Y-%m-%d %H:%M:%S"))
+
+end_time = "2016-11-11 23:59:59"
+end_time_s = time.mktime(time.strptime(end_time, "%Y-%m-%d %H:%M:%S"))
+
+# 剩余开始/结束时间
+def leftStartTime():
+    return start_time_s - time.time()
+
+def leftEndTime():
+    return end_time_s - time.time()
+
+# 是否已经开始
+def isStart():
+    left = leftStartTime()
+    if left > 0:
+        return False
+    return True
+
+# 是否已经结束
+def isEnd():
+    if isStart():
+        left = leftEndTime()
+        if left >= 0:
+            return True
+    return False
+
